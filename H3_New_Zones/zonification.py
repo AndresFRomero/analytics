37dd1ba5b4542b48c44862d4e31ed198d74b8edf
AndresRomero <aromero@soytul.com>
2022-07-28 19:15:18 -0500

    Migracion CP a H3

--- H3_New_Zones/zonification.py
+++ H3_New_Zones/zonification.py
@@ -1,6 +1,6 @@
 import kml2geojson
 import json
-jsonDict = kml2geojson.main.convert('CP_15Mex_v8.kml')[0]
+jsonDict = kml2geojson.main.convert('CP_14Jal_v8.kml')[0]
 
-with open('MEXICO.geojson', 'w') as fp:
+with open('JALISCO.geojson', 'w') as fp:
     json.dump(jsonDict, fp)
